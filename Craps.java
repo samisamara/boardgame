bf512683e4e305ce6fb6ccbd6ce1cad7c7bdb867
davidvilla62 <77689256+davidvilla62@users.noreply.github.com>
2021-03-17 12:18:35 -0500

    Create Craps.java

---
+++ Craps.java
@@ -0,0 +1,60 @@
+public static double craps(Double money) {
+        //initialize variables
+        boolean play = true;
+        Double mon = money;
+        Double bet = 0.0;
+        System.out.println(mon);
+        Scanner scnr = new Scanner(System.in);
+        Random rnd = new Random();
+       
+            //asks user to enter help to see rules or continue
+            System.out.println("     Welcome to Craps!");
+            System.out.println("To see rules enter -Help or -No to continue");
+            String askHelp = scnr.next();
+            if (askHelp.equalsIgnoreCase("help")) {
+                help();
+            } 
+            //enter bet before dice roll, if bet > player money, player must enter another bet
+            else { 
+                while (play == true) {
+                System.out.println("To begin enter your bet!");
+                bet = scnr.nextDouble();
+                while (bet > mon) {
+                    System.out.println("Bet can't be more than Player Money enter again:");
+                    bet = scnr.nextDouble();
+                }
+            
+            //dice roll
+            int die1 = rnd.nextInt(6) + 1;
+            int die2 = rnd.nextInt(6) + 1;
+            int total = die1 + die2;
+            //checks if win
+            int[] win = {2, 3, 4, 10, 11, 12};
+            int[] lose = {5, 6, 7, 8, 9};
+            for (int i = 0; i < 6; i++) {
+                if (total == win[i]) {
+                    System.out.println("Dice Roll: " + total);
+                    bet = bet * 2;
+                    System.out.println("YOU WIN: $" + bet);
+                    mon= mon+bet;
+                }
+            }
+            for (int k = 0; k < 5; k++) {
+                if (total == lose[k]) {
+                    System.out.println("Dice Roll: " + total);
+                    System.out.println("YOU LOSE: $" + bet);
+                    mon = mon - bet;
+                }
+            }
+            System.out.println("Player Money: $"+mon);
+            
+                    System.out.println("Want to play again? Enter: Y/N");
+                    String ans = scnr.next();
+                    if(ans.equalsIgnoreCase("n")){
+                        play = false;
+                    }
+        }
+        
+    }
+    return mon;        
+}
